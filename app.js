c7b06dd30e3077b0fee4101ae1888f010dadd3ec
Ziyovuddin <z.shamsiddini@gmail.com>
2017-11-10 23:25:35 -0500

    form prototype

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
 const express = require('express');
 const hbs = require('hbs');
-var request = require('request');
+const request = require('request');
+var bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 var port = process.env.PORT || 3000;
 app.set('view engine', 'hbs');
 
 app.get('/', function(req, res){
-  request({
-        url: `http://api.reimaginebanking.com/accounts?key=4dbdf27c939b12bc9ccb1ab97e7a800c`,
-        json: true
-      }, function(error, response, body){
-        if(!error){
-          res.send(body);
-        }
-      });
+  // request({
+  //       url: `http://api.reimaginebanking.com/accounts?key=4dbdf27c939b12bc9ccb1ab97e7a800c`,
+  //       json: true
+  //     }, function(error, response, body){
+  //       if(!error){
+  //         var arr = Array.from(body.results);
+  //         var newArr = arr.filter(function(x){
+  //           return x.type == "Checking" && x.balance > 40000;
+  //         });
+  //         res.send(newArr);
+  //       }else{
+  //         console.log(error);
+  //       }
+  //     });
+
+  res.render('index.hbs');
 });
 
-
-
-
-
-
+app.post('/form', function(req, res){
+  res.render('form.hbs', {
+    username: req.body.user_name,
+    email: req.body.user_mail,
+    message: req.body.user_message
+  });
+});
 
 app.listen(port, function(){
   console.log(`Success, its running on port ${port}`);
